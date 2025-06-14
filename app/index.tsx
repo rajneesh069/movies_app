5998661dba254b5232e3ee11c2cda0a4e7cc3ad2
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2025-06-14 18:01:04 +0530

    init

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center content-center">
+      <Text className="text-5xl text-green-900">Hello Hi</Text>
+    </View>
+  );
+}
