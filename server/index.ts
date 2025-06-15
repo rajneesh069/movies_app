6fb744be05d1565a5d6539cf5e7dd92a4e27daac
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2025-06-15 17:43:09 +0530

    add /search/movie and /discover/movie

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,10 @@ import dotenv from "dotenv";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 import cors from "cors";
+
 // Shim __filename and __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
 
 console.log("env Path:", resolve(__dirname, "../.env"));
 dotenv.config({ path: resolve(__dirname, "../.env") });
@@ -31,13 +32,49 @@ if (!TMDB_ACCESS_TOKEN) {
   );
 }
 
-app.get("/", async (_, res) => {
+app.get("/", async (_, res: Response) => {
   res.json({ message: "Server is running properly." }).status(200);
 });
 
-app.get("/discover/movie", async (_, res) => {
+app.get("/search/movie", async (req: Request, res: Response) => {
+  const query = req.query.query as string;
+  if (!query) {
+    return res
+      .status(400)
+      .json({
+        success: false,
+        message: "Missing `query` parameter",
+        data: null,
+      });
+  }
+
+  const url = `https://api.themoviedb.org/3/search/movie?include_adult=false&query=${encodeURIComponent(
+    query
+  )}`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return res
+      .status(200)
+      .json({ success: true, message: "Call Succeeded", data: result });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ success: false, message: (err as Error).message });
+  }
+});
+
+app.get("/discover/movie", async (_, res: Response) => {
   const url =
-    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc";
   const options = {
     method: "GET",
     headers: {
