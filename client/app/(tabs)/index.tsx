1c35109830e1589212cc934d9c270939662fff14
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2025-06-15 20:29:16 +0530

    add the searchbar and hook it to the Search tab

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,8 +1,20 @@
+import Searchbar from "@/components/Searchbar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, View } from "react-native";
+import { fetchMovies } from "@/services/api";
+import { useFetch } from "@/services/useFetch";
+import { useRouter } from "expo-router";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+    refetch,
+  } = useFetch(() => fetchMovies({ query: "" }));
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute z-0 w-full" />
@@ -15,6 +27,23 @@ export default function Index() {
         }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        {moviesLoading ? (
+          <ActivityIndicator
+            size={"large"}
+            color={"#0000FF"}
+            className="mt-10 self-center"
+          />
+        ) : moviesError ? (
+          <Text>Error : {moviesError.message}</Text>
+        ) : (
+          <View>
+            <Searchbar
+              placeholder="Search for a movie"
+              onPress={() => router.push("/search")}
+            />
+          </View>
+        )}
       </ScrollView>
     </View>
   );
